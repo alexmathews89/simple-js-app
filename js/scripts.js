e74bef58f4a27e625121dbc8fb10bdcc280ee5f3
alexmathews89 <alexmathews@live.com>
2024-01-07 17:06:30 -0800

    added a comment to describe the array

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,5 @@ let pokemonList = [
   { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
   { name: "Articuno", height: 1.7, type: ["ice", "flying"] },
 ];
+
+// the array listed above is a list of pokemon
