d2724392e45150562b676930ade53da2aaafa33c
alexmathews89 <alexmathews@live.com>
2024-01-07 16:54:47 -0800

    changed Ninetales to Articuno in the Pokemon List

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
   { name: "Bulbasaur", heitht: 0.7, type: ["grass", "poison"] },
   { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
-  { name: "Ninetales", height: 1.1, type: ["fire"] },
+  { name: "Articuno", height: 1.7, type: ["ice", "flying"] },
 ];
