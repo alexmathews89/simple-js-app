08ec83b3b76d436cd76edf815425e93342598d6e
alexmathews89 <alexmathews@live.com>
2024-01-12 21:43:25 -0800

    changed Atricuno's height

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
   { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
   { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-  { name: "Articuno", height: 1.7, types: ["ice", "flying"] },
+  { name: "Articuno", height: 1.5, types: ["ice", "flying"] },
 ];
 
 // the array listed above is a list of pokemon
