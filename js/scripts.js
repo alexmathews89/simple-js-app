618e09d3492248961abf46159dea0d05ea2c8d18
alexmathews89 <alexmathews@live.com>
2024-01-12 20:08:21 -0800

    corrected typos

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-  { name: "Bulbasaur", heitht: 0.7, type: ["grass", "poison"] },
-  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
-  { name: "Articuno", height: 1.7, type: ["ice", "flying"] },
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+  { name: "Articuno", height: 1.7, types: ["ice", "flying"] },
 ];
 
 // the array listed above is a list of pokemon
