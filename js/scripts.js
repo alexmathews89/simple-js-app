9a2a5cebb1f32c6de55dc7c164e7fdcb1b42bccb
alexmathews89 <alexmathews@live.com>
2024-01-16 22:17:47 -0800

    wrapped pokemonList into an IIFE and assigned to pokemonRepository variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-  { name: "Articuno", height: 1.5, types: ["ice", "flying"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+    { name: "Articuno", height: 1.5, types: ["ice", "flying"] },
+  ];
+})();
 
 // the array listed above is a list of pokemon
 
