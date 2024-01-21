16ab3fdec0834649bac8f04e2acd00287baacbb3
alexmathews89 <alexmathews@live.com>
2024-01-21 11:00:39 -0800

    added UL list, commented out doc.write code and declared JS variables

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,15 @@ let pokemonRepository = (function () {
 // the array listed above is a list of pokemon
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write(" " + pokemon.name + "(height: " + pokemon.height + ")");
-  if (pokemon.height > 1.5) {
-    document.write(" -Wow, that's big!");
-  }
+  //document.write(" " + pokemon.name + "(height: " + pokemon.height + ")");
+  //if (pokemon.height > 1.5) {
+  //document.write(" -Wow, that's big!");
+
+  //}
+
+  let pokemonUnorderedList = document.querySelector(".pokemon-list");
+  let listItem = document.createElement("li");
+  let button = document.createElement("button");
+  button.innerText = pokemon.name;
+  button.classList.add("button-class");
 });
