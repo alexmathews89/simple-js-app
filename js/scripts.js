da4b4fe375ce0fd2d9932eaf741323f0fd9f14a8
alexmathews89 <alexmathews@live.com>
2024-01-21 11:23:10 -0800

    used JS append child function for new elements

--- js/scripts.js
+++ js/scripts.js
@@ -32,4 +32,6 @@ pokemonRepository.getAll().forEach(function (pokemon) {
   let button = document.createElement("button");
   button.innerText = pokemon.name;
   button.classList.add("button-class");
+  listItem.appendChild(button);
+  pokemonUnorderedList.appendChild(listItem);
 });
