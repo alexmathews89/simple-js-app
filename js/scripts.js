fa836559540efe0790dc06d279a20037d6ec9fc0
alexmathews89 <alexmathews@live.com>
2024-01-23 23:46:53 -0800

    added load list function and assigned to a key

--- js/scripts.js
+++ js/scripts.js
@@ -32,13 +32,29 @@ let pokemonRepository = (function () {
         return response.json();
       })
       .then(function (json) {
-        json.results.forEach(function (itme) {
+        json.results.forEach(function (item) {
           let pokemon = {
-            name: itme.name,
-            detailsUrl: itme.url,
+            name: item.name,
+            detailsUrl: item.url,
           };
           add(pokemon);
         });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
       })
       .catch(function (e) {
         console.error(e);
@@ -49,17 +65,25 @@ let pokemonRepository = (function () {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
+
 // the array listed above is a list of pokemon
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  //document.write(" " + pokemon.name + "(height: " + pokemon.height + ")");
-  //if (pokemon.height > 1.5) {
-  //document.write(" -Wow, that's big!");
+//pokemonRepository.getAll().forEach(function (pokemon) {
+//document.write(" " + pokemon.name + "(height: " + pokemon.height + ")");
+//if (pokemon.height > 1.5) {
+//document.write(" -Wow, that's big!");
 
-  //}
+//}
 
-  pokemonRepository.addListItem(pokemon);
-});
+//pokemonRepository.addListItem(pokemon);
+//});
